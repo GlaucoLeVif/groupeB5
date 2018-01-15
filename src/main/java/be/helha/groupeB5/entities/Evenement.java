614a9d2367c804c4829aaa1b0dccba5bd39f610d
Victor <victor.papleux@gmail.com>
2018-01-15 11:26:07 +0100

    Petit potam, issou!

---
+++ src/main/java/be/helha/groupeB5/entities/Evenement.java
@@ -0,0 +1,11 @@
+package be.helha.groupeB5.entities;
+
+import java.util.Date;
+
+public class Evenement {
+
+	private String titre, resume, urlImage;
+	private double objectif, recolte;
+	private Date dateEv;
+	
+}
