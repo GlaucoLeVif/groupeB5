ebd2cdbf1e6d29ec369600e6df41c483a68eccf1
Florian <Florian@DESKTOP-T3H8J89>
2018-01-15 12:54:31 +0100

    aaa

--- src/main/java/be/helha/groupeB5/controller/InscriptionControl.java
+++ src/main/java/be/helha/groupeB5/controller/InscriptionControl.java
@@ -2,17 +2,131 @@ package be.helha.groupeB5.controller;
 
 import java.util.Date;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+
+import be.helha.groupeB5.entities.Membre;
 
 @Named
 @RequestScoped
 public class InscriptionControl {
 
+	@EJB
+	private GestionMembreEJB gestionMembreEJB;
 	private String login, mdp, mail, prenom, nom, rue, ville, pays, nationalite, numeroGSM;
 	private Date dateN;
 	private int numRue, cp;
 	private boolean isAdmin;
 	
 	public InscriptionControl() {}
+	
+	
+	public Membre doAjouterMembre() {
+		Membre m = new Membre(login, mdp);
+		return gestionMembreEJB.ajouterMembre(m);
+	}
+	
+	
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+
+	public String getNationalite() {
+		return nationalite;
+	}
+	public void setNationalite(String nationalite) {
+		this.nationalite = nationalite;
+	}
+
+	public String getNumeroGSM() {
+		return numeroGSM;
+	}
+	public void setNumeroGSM(String numeroGSM) {
+		this.numeroGSM = numeroGSM;
+	}
+
+	public Date getDateN() {
+		return dateN;
+	}
+	public void setDateN(Date dateN) {
+		this.dateN = dateN;
+	}
+
+	public int getNumRue() {
+		return numRue;
+	}
+	public void setNumRue(int numRue) {
+		this.numRue = numRue;
+	}
+
+	public int getCp() {
+		return cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+	
+	
 }
