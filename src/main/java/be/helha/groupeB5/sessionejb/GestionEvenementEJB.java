d39f1559d856494469a99d494540c1403c372b60
Alexandre <alexandre.adriaens@gmail.com>
2018-01-16 20:39:36 +0100

    on peut voir les images sur le site à partir de la bd hihi

--- src/main/java/be/helha/groupeB5/sessionejb/GestionEvenementEJB.java
+++ src/main/java/be/helha/groupeB5/sessionejb/GestionEvenementEJB.java
@@ -3,10 +3,14 @@ package be.helha.groupeB5.sessionejb;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 
 import be.helha.groupeB5.dao.DAOMembreLocalBean;
 import be.helha.groupeB5.entities.Evenement;
 
+@Stateless
+@LocalBean
 public class GestionEvenementEJB implements IGestionEvenementEJBRemote{
 
 	@EJB
