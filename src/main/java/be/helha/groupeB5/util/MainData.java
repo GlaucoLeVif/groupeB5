19dc10dd81b0e54e9cddd52041570cce7bf2aa89
Alexandre <alexandre.adriaens@gmail.com>
2018-01-16 11:14:15 +0100

    date fonctionne

--- src/main/java/be/helha/groupeB5/util/MainData.java
+++ src/main/java/be/helha/groupeB5/util/MainData.java
@@ -1,7 +1,10 @@
 package be.helha.groupeB5.util;
 
 
-import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,14 +19,31 @@ import be.helha.groupeB5.entities.Participation;
 public class MainData {
 
 	public static void main(String[] args) {
-
-		Date d = new Date(1997, 4, 14);
-		Membre m = new Membre("log", "mdp", "monmail", "distant", "Led", "place", "Beaumont", "Belgique", "BE", "0492", d, 1, 6500, false);	// PQ numGSM en String
-		Evenement e = new Evenement("titre1", "resume1", "url1", 5000.00, 0, new Date(2018,7,14));
-		Evenement e2 = new Evenement("titre2", "resume2", "url2", 100000.00, 0, new Date(2018,8,12));
+		Date d = new Date();
+		Date d2 = new Date();
+		Date d3 = new Date();
+		Date d4 = new Date();
+		Date d5 = new Date();
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			d = format.parse("05/02/1997");
+			d2 = format.parse("01/02/2018");
+			d3 = format.parse("02/02/2018");
+			d4 = format.parse("01/01/2018");
+			d5 = format.parse("02/01/2018");
+			
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		Participation p = new Participation(20, new Date(2018,2,02),m,e);
-		Participation p2 = new Participation(40, new Date(2018,3,04),m,e2);
+	
+		Membre m = new Membre("log", "mdp", "monmail", "distant", "Led", "place", "Beaumont", "Belgique", "BE", "0492", d, 1, 6500, false);	// PQ numGSM en String
+		Evenement e = new Evenement("titre1", "resume1", "url1", 5000.00, 0, d2);
+		Evenement e2 = new Evenement("titre2", "resume2", "url2", 100000.00, 0, d3);
+		
+		Participation p = new Participation(20, d4,m,e);
+		Participation p2 = new Participation(40, d5,m,e2);
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("groupeB5");
 		EntityManager em = emf.createEntityManager();
