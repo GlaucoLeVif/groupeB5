5e77d99b5f3479e928eb1381e3ea20d95d12c5d6
Alexandre <alexandre.adriaens@gmail.com>
2018-01-17 22:27:30 +0100

    Tentative d'email

--- src/main/java/be/helha/groupeB5/controller/EvenementController.java
+++ src/main/java/be/helha/groupeB5/controller/EvenementController.java
@@ -23,6 +23,7 @@ import com.sun.istack.logging.Logger;
 
 import be.helha.groupeB5.entities.Evenement;
 import be.helha.groupeB5.entities.Image;
+import be.helha.groupeB5.entities.MailGestion;
 import be.helha.groupeB5.entities.Membre;
 import be.helha.groupeB5.entities.Participation;
 import be.helha.groupeB5.entities.UploadPage;
@@ -43,6 +44,7 @@ public class EvenementController {
 	private byte[] image1;
 	private File file1;
 	private UploadPage up = new UploadPage();
+	private MailGestion mailG = new MailGestion();
 	
 	public EvenementController() {}
 	
@@ -244,7 +246,10 @@ public class EvenementController {
 	}
 	
 	
-	
+	public void sendEmail()
+	{
+		mailG.sendMail();
+	}
 	
 	
 	
