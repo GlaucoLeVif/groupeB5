b6f9141560c6a0f8208256da51bb227ddb0534d8
Alexandre <alexandre.adriaens@gmail.com>
2018-01-17 22:44:38 +0100

    email fonctionne

--- src/main/java/be/helha/groupeB5/entities/MailGestion.java
+++ src/main/java/be/helha/groupeB5/entities/MailGestion.java
@@ -64,11 +64,11 @@ public class MailGestion implements Serializable {
 		             message.setFrom(new InternetAddress("alex.10101997@gmail.com"));
 		             message.setRecipients(Message.RecipientType.TO,
 		             InternetAddress.parse("alex.10101997@gmail.com"));
-		             message.setSubject("Test JCG Example");
-		             message.setText("Hi, This is a Test mail for JCG Example!");
+		             message.setSubject("Notif - Ajout d'un evenement - ");
+		             message.setText("Un nouvel événement a été créé par - - au sujet de - - et se déroule - - . Pour gérer vos événements, rendez-vous sur le site dans la section gérer les événements : --");
 		             Transport.send(message);
 		 
-		             System.out.println("Mail sent succesfully!");
+		             System.out.println("Envoyé");
 		 
 		  
 		 
@@ -78,6 +78,77 @@ public class MailGestion implements Serializable {
 		 
 		         }
 
+/*
+		final String username = "servicemsfmail@gmail.com";
+        final String password = "msf123456";
+  
+		
+		 
+		
+		        Properties props = new Properties();
+		
+		        props.put("mail.smtp.host", "smtp.gmail.com");
+		
+		        props.put("mail.smtp.socketFactory.port", "465");
+		
+		        props.put("mail.smtp.socketFactory.class",
+		
+		                "javax.net.ssl.SSLSocketFactory");
+		
+		        props.put("mail.smtp.auth", "true");
+		
+		        props.put("mail.smtp.port", "465");
+		
+		 
+		
+		        Session session = Session.getDefaultInstance(props,
+		
+		                new javax.mail.Authenticator() {
+		
+		                    protected PasswordAuthentication getPasswordAuthentication() {
+		
+		                       return new PasswordAuthentication(username,password);
+		
+		                    }
+		
+		                });
+		
+		 
+		
+		        try {
+		
+	
+		
+		            Message message = new MimeMessage(session);
+		
+		            message.setFrom(new InternetAddress("alex.10101997@gmail.com"));
+		
+		            message.setRecipients(Message.RecipientType.TO,
+		
+		                    InternetAddress.parse("alex.10101997@gmail.com"));
+		
+		            message.setSubject("Test JCG Example");
+		
+		            message.setText("Hi," +
+		
+		                    "This is a Test mail for JCG Example!");
+		
+		 
+		
+		            Transport.send(message);
+		
+		 
+		
+		            System.out.println("Mail sent succesfully!");
+		
+		 
+		
+		        } catch (MessagingException e) {
+		
+		           throw new RuntimeException(e);
+		
+		        }
+*/
 
 	}
 
